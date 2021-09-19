b69072246c0a92b05e791e869e4140fa94e59fc0
Formerscholar <2319315514@qq.com>
2021-09-19 15:50:21 +0800

    一键注释

--- src/main/java/com/chad/aopdemo/controller/AopContriller.java
+++ src/main/java/com/chad/aopdemo/controller/AopContriller.java
@@ -4,10 +4,22 @@ import com.chad.aopdemo.aop.inter.NewWebLog;
 import com.chad.aopdemo.aop.inter.WebLog;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * aop contriller
+ *
+ * @author Administrator
+ * @date 2021/09/19
+ */
 @RestController
 @RequestMapping("aop")
 public class AopContriller {
 	
+	/**
+	 * aop演示
+	 *
+	 * @param id id
+	 * @return {@link String}
+	 */
 	@GetMapping("demo")
 	//@WebLog(desc = "测试aop方法")
 	@NewWebLog(desc = "测试aop方法")
