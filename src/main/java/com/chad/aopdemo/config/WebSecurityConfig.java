5061b15c16f9d8531adda45ae940bcd38f5067d6
Formerscholar <2319315514@qq.com>
2021-09-22 19:42:56 +0800

    dengl

--- src/main/java/com/chad/aopdemo/config/WebSecurityConfig.java
+++ src/main/java/com/chad/aopdemo/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,6 +14,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private MyUserDetailsService myUserDetailsService;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.formLogin().loginPage("/login.html");
+		http.authorizeRequests().antMatchers("/login.html").permitAll().anyRequest().authenticated();
+	}
 	
 	//配置用户
 	@Override
